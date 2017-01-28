44517e78fc6be8a7a656edc75c80620912fc62d5
David Tran <David Tran>
2017-01-28 16:55:01 -0500

    install pug

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var app = express();
 
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
+app.set('view engine', 'pug');
+/*
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+*/
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.listen(app.get('port'), function() {
